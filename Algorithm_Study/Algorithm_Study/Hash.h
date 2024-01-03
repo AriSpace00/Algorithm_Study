b39ad3ece621e9e5ce7bdcd0141ce83501fcc121
AriSpace00 <dhjj2110@gmail.com>
2024-01-03 16:16:14 +0900

    [hash] string
    - stringData 지원되도록 Insert 함수, Show 함수 수정 중

--- Algorithm_Study/Algorithm_Study/Hash.h
+++ Algorithm_Study/Algorithm_Study/Hash.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
+#include <string>
 #include <iostream>
+
+template <typename T>
 class Hash
 {
 public:
@@ -8,47 +11,79 @@ public:
     ~Hash();
 
 private:
-    std::vector<std::vector<int>> m_data;
+    std::vector<std::vector<T>> m_typeData;
+    std::vector<std::vector<std::string>> m_stringData;
 
 public:
-    void Insert(int value);
+    void Insert(T value);
+    void Insert(std::string value);
     void Find(int key);
     void Erase(int key);
-    void Show();
+    void ShowTypeData();
+    void ShowStringData();
 };
 
-inline Hash::Hash()
+template <typename T>
+inline Hash<T>::Hash()
 {
-    m_data.resize(8191);
+    m_typeData.resize(8191);
+    m_stringData.reserve(8191);
 }
 
-inline Hash::~Hash()
-{
-}
-
-void Hash::Insert(int value)
-{
-    int index = (((5381 << 5) + 5381) + value) % 8191;
-    m_data[index].push_back(value);
-}
-
-inline void Hash::Find(int key)
+template <typename T>
+inline Hash<T>::~Hash()
 {
 }
 
-inline void Hash::Erase(int key)
+template <typename T>
+void Hash<T>::Insert(T value)
+{
+    int index = (((5381 << 5) + 5381) + (int)value) % 8191;
+    m_typeData[index].push_back(value);
+}
+
+template <>
+void Hash<std::string>::Insert(std::string value)
+{
+    int index = (((5381 << 5) + 5381) + value.length()) % 8191;
+    m_stringData[index].push_back(value);
+}
+
+template <typename T>
+inline void Hash<T>::Find(int key)
 {
 }
 
-inline void Hash::Show()
+template <typename T>
+inline void Hash<T>::Erase(int key)
 {
-    for (int i = 0; i < m_data.size(); i++)
+}
+
+template <typename T>
+inline void Hash<T>::ShowTypeData()
+{
+    for (int i = 0; i < m_typeData.size(); i++)
     {
-        for (int j = 0; j < m_data[i].size(); j++)
+        for (int j = 0; j < m_typeData[i].size(); j++)
         {
-            if (m_data[i][j] != 0)
+            if (m_typeData[i][j] != 0)
             {
-                std::cout << "[" << i << "][" << j << "] : " << m_data[i][j] << std::endl;
+                std::cout << "[" << i << "][" << j << "] : " << m_typeData[i][j] << std::endl;
+            }
+        }
+    }
+}
+
+template <typename T>
+void Hash<T>::ShowStringData()
+{
+    for (int i = 0; i < m_stringData.size(); i++)
+    {
+        for (int j = 0; j < m_stringData[i].size(); j++)
+        {
+            if (!m_stringData[i][j].empty())
+            {
+                std::cout << "[" << i << "][" << j << "] : " << m_stringData[i][j] << std::endl;
             }
         }
     }
--- Algorithm_Study/Algorithm_Study/Main.cpp
+++ Algorithm_Study/Algorithm_Study/Main.cpp
@@ -1,15 +1,54 @@
 #include "Hash.h"
-#include <string>
+
+#include <fstream>
 
 int main()
 {
-    Hash* hash = new Hash();
-    hash->Insert(1);
-    hash->Insert(2);
-    hash->Insert(3);
-    hash->Insert(4);
-    hash->Insert(5);
-    hash->Insert(6);
+    std::cout << "english3.txt" << std::endl;
 
-    hash->Show();
+    Hash<std::string> stringHash;
+    std::fstream file;
+    std::string word;
+    std::string filePath = "../Resource/english3.txt";
+    file.open(filePath.c_str());
+    while(file >> word)
+    {
+        stringHash.Insert(word);
+    }
+
+    stringHash.ShowStringData();
+
+    /*std::cout << "int" << std::endl;
+    Hash<int> hash1;
+    hash1.Insert(1);
+    hash1.Insert(2);
+    hash1.Insert(3);
+    hash1.Insert(4);
+    hash1.Insert(5);
+    hash1.Insert(6);
+    hash1.ShowTypeData();
+
+    std::cout << std::endl;
+
+    std::cout << "char" << std::endl;
+    Hash<char> hash2;
+    hash2.Insert('a');
+    hash2.Insert('b');
+    hash2.Insert('c');
+    hash2.Insert('d');
+    hash2.Insert('e');
+    hash2.Insert('f');
+    hash2.ShowTypeData();
+
+    std::cout << std::endl;
+
+    std::cout << "float" << std::endl;
+    Hash<float> hash3;
+    hash3.Insert(1.1f);
+    hash3.Insert(2.1f);
+    hash3.Insert(3.1f);
+    hash3.Insert(4.1f);
+    hash3.Insert(5.1f);
+    hash3.Insert(6.1f);
+    hash3.ShowTypeData();*/
 }
